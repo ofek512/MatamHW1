8857a9354468ea88e1bd148c5cd029c38bc7b0d4
Ofek Yankis <95701220+ofek512@users.noreply.github.com>
2024-06-19 16:56:41 +0300

    Adding transactions.cpp comments to each function

--- transactions.cpp
+++ transactions.cpp
@@ -22,10 +22,25 @@ void TransactionDumpInfo(const Transaction &transaction, ofstream &file) {
     file << "Receiver Name: " << transaction.receiver << "\n";
     file << "Transaction Value: " << transaction.value << "\n";
 }
+/**
+ * TransactionHashMessage - Hashs the message of the transaction
+ *
+ * @param transaction Transaction to hash
+ *
+ * @return The hashed message
+*/
 string TransactionHashedMessage(const Transaction& transaction){
     return hash(transaction.value, transaction.sender, transaction.receiver);
 }
-
+/**
+ * TransactionVerifyHashedMessage - Verifies that a given transaction suits a given hashed message
+ *
+ * @param transaction Given transaction
+ * @param hashedMessage Hashed message to verify
+ *
+ * @return true if the message given is suitable to this transaction, false otherwise
+ *
+*/
 bool TransactionVerifyHashedMessage(
         const Transaction& transaction,
         string hashedMessage
