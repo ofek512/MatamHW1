4aae86d66cd5b6d222ecc68921781016e4284b13
Ofek Yankis <95701220+ofek512@users.noreply.github.com>
2024-06-21 18:24:28 +0300

    changing utilities.cpp to the updated file from the task material

--- Utilities.cpp
+++ Utilities.cpp
@@ -4,16 +4,17 @@
 
 using std::string;
 
+
 string hash(int key, const string& value1, const string& value2) {
     static const int out_size = 20;
     char bytes[out_size + 1] = {0};
     for (int i = 0; i < out_size; i++) {
         bytes[i] = key ^ (out_size - i);
     }
-    for (int i = 0; i < value1.size() * out_size ; i++) {
+    for (size_t i = 0; i < value1.size() * out_size ; i++) {
         bytes[i % (out_size/2)] ^= value1[i % value1.size()] ^ i;
     }
-    for (int i = 0; i < value2.size() * out_size ; i++) {
+    for (size_t i = 0; i < value2.size() * out_size ; i++) {
         bytes[(out_size/2) + (i % out_size/2)] ^= value2[i % value2.size()] ^ i;
     }
     for (int i = 0; i < out_size; i++) {
