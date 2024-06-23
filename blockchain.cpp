54d765f901ecf30e68fc41218d2eab3f05c9fd54
Ofek <ofek5202@gmail.com>
2024-06-23 17:05:46 +0300

    adding BlockChainCompress function c:

--- blockchain.cpp
+++ blockchain.cpp
@@ -52,12 +52,8 @@ void BlockChainAppendTransaction(
         const string &receiver,
         const string &timestamp
 ) {
-    Transaction newTrans;
-    newTrans.value = value;
-    newTrans.sender = sender;
-    newTrans.receiver = receiver;
     auto *newNode = new BlockChainNode;
-    newNode->transaction = newTrans;
+    newNode->transaction = {value, sender, receiver};
     newNode->previous = blockChain.head;
     newNode->timestamp = timestamp;
     blockChain.head = newNode;
@@ -75,7 +71,7 @@ void BlockChainAppendTransaction(
         const Transaction &transaction,
         const string &timestamp
 ) {
-    auto* newNode = new BlockChainNode;
+    auto *newNode = new BlockChainNode;
     newNode->transaction = transaction;
     newNode->previous = blockChain.head;
     newNode->timestamp = timestamp;
@@ -92,7 +88,7 @@ void BlockChainAppendTransaction(
 
 int BlockChainGetSize(const BlockChain &blockChain) {
     int counter = 0;
-    BlockChainNode* node = blockChain.head;
+    BlockChainNode *node = blockChain.head;
     while (node != nullptr) {
         node = node->previous;
         counter++;
@@ -108,16 +104,16 @@ int BlockChainGetSize(const BlockChain &blockChain) {
  * @return BlockChain created from the file
  *
 */
-BlockChain BlockChainLoad(ifstream& file){
+BlockChain BlockChainLoad(ifstream &file) {
     BlockChain blockchain, temp_blockchain;
     blockchain.head = nullptr;
     temp_blockchain.head = nullptr;
     string sender, receiver, time_stamp;
     int sum;
-    while(file >> sender >> receiver >> sum >> time_stamp){
-        BlockChainAppendTransaction(temp_blockchain,sum, sender, receiver, time_stamp);
+    while (file >> sender >> receiver >> sum >> time_stamp) {
+        BlockChainAppendTransaction(temp_blockchain, sum, sender, receiver, time_stamp);
     }
-    for(int i = 0; i < BlockChainGetSize(temp_blockchain);i++){
+    for (int i = 0; i < BlockChainGetSize(temp_blockchain); i++) {
         BlockChainAppendTransaction(blockchain, temp_blockchain.head->transaction, temp_blockchain.head->timestamp);
         temp_blockchain.head = temp_blockchain.head->previous;
     }
@@ -140,11 +136,11 @@ BlockChain BlockChainLoad(ifstream& file){
  * @param file File to print to
  *
 */
-void BlockChainDump(const BlockChain& blockChain, ofstream& file){
-    auto* current_node = blockChain.head;
+void BlockChainDump(const BlockChain &blockChain, ofstream &file) {
+    auto *current_node = blockChain.head;
     int counter = 1;
     file << "BlockChain info:" << endl;
-    while(current_node != nullptr){
+    while (current_node != nullptr) {
         file << counter << "." << endl;
         file << "Sender name: " << current_node->transaction.sender << endl;
         file << "Receiver name: " << current_node->transaction.receiver << endl;
@@ -153,6 +149,7 @@ void BlockChainDump(const BlockChain& blockChain, ofstream& file){
         current_node = current_node->previous;
     }
 }
+
 /**
  * BlockChainDumpHashed - Prints the *hashed data* of all transactions in the BlockChain to a given file
  *
@@ -166,14 +163,15 @@ void BlockChainDump(const BlockChain& blockChain, ofstream& file){
  * @param file File to print to
  *
 */
-void BlockChainDumpHashed(const BlockChain& blockChain, ofstream& file){
-    auto* current_node = blockChain.head;
-    for(int i = 1; i < BlockChainGetSize(blockChain); i++){
+void BlockChainDumpHashed(const BlockChain &blockChain, ofstream &file) {
+    auto *current_node = blockChain.head;
+    for (int i = 1; i < BlockChainGetSize(blockChain); i++) {
         file << TransactionHashedMessage(current_node->transaction) << endl;
         current_node = current_node->previous;
     }
     file << TransactionHashedMessage(current_node->transaction);
 }
+
 /**
  * BlockChainVerifyFile - verifies that the file contains correct hashed messages of the given BlockChain
  *
@@ -188,16 +186,37 @@ void BlockChainDumpHashed(const BlockChain& blockChain, ofstream& file){
  *
  * @return true if the file is valid, false otherwise
 */
-bool BlockChainVerifyFile(const BlockChain& blockChain, std::ifstream& file){
-    auto* current_node = blockChain.head;
+bool BlockChainVerifyFile(const BlockChain &blockChain, std::ifstream &file) {
+    auto *current_node = blockChain.head;
     string current_hashcode;
-    while(file >> current_hashcode){
-        if(current_node == nullptr || current_hashcode != TransactionHashedMessage(current_node->transaction))
+    while (file >> current_hashcode) {
+        if (current_node == nullptr || current_hashcode != TransactionHashedMessage(current_node->transaction))
             return false;
         current_node = current_node->previous;
     }
-    if(current_node!= nullptr)
+    if (current_node != nullptr)
         return false;
     return true;
 }
 
+/**
+ * BlockChainCompress - Compresses the given block chain based on the transaction's data.
+ * All consecutive blocks with the same sender and receiver will be compressed to one Block.
+ *
+ * @param blockChain BlockChain to compress
+*/
+void BlockChainCompress(BlockChain &blockChain) {
+    auto *looper = blockChain.head;
+    BlockChainNode *temp;
+    while (looper->previous != nullptr) {
+        temp = looper->previous;
+        if (looper->transaction.sender == looper->previous->transaction.sender
+            && looper->transaction.receiver == looper->previous->transaction.receiver) {
+            looper->transaction.value += looper->previous->transaction.value;
+            looper->timestamp = looper->previous->timestamp;
+            looper->previous = looper->previous->previous;
+            delete[] temp;
+        }
+    }
+
+}
